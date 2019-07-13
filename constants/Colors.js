89dc33060bbc54828e3644b0b72918a05b82bb51
Tyler Swartz <tmswartz12@gmail.com>
2019-07-12 22:05:04 -0400

    style: added team colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -10,5 +10,17 @@ export default {
   warningBackground: '#EAEB5E',
   warningText: '#666804',
   noticeBackground: tintColor,
-  noticeText: '#fff'
+  noticeText: '#fff',
+  archers: '#F3694C',
+  whipsnakesPrimary: '#E81A3E',
+  whipsnakesSecondary: '#2C3830',
+  chromePrimary: '#95D6DC',
+  chromeSecondary: '#E474A2',
+  chaosPrimary: '#ED4949',
+  chaosSecondary: 'black',
+  redwoodsPrimary: '#186F56', //green
+  atlasPrimary: '#229CC7',
+  atlasSecondary: '#2F2E4E',
+  archersPrimary: '#222D40',
+  archersSecondary: '#F2684B'
 }

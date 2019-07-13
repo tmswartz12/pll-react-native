1fa45d12390e4d64f75312684465e7738c847d85
Tyler Swartz <tmswartz12@gmail.com>
2019-07-12 22:11:22 -0400

    err: 403 error on CDM

--- components/IndividualTeam.js
+++ components/IndividualTeam.js
@@ -1,38 +1,52 @@
 import React from 'react'
 import { ScrollView, StyleSheet, Text } from 'react-native'
 import axios from 'axios'
+import Colors from '../constants/Colors'
 
 export default class IndividualTeam extends React.Component {
   state = {
     players: []
   }
   static navigationOptions = ({ navigation }) => {
+    const title = navigation.getParam('headerTitle', 'Team')
+
     return {
-      title: navigation.getParam('headerTitle', 'Team')
+      title,
+      headerStyle: {
+        backgroundColor: Colors[`${title.toLowerCase()}Primary`]
+      },
+      //headerTintColor: Colors[`${title.toLowerCase()}Secondary`],
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
     }
   }
   async componentDidMount() {
     const teamId = this.props.navigation.getParam('teamId')
-    const { data } = await axios.get(
-      `https://dn0a11v09sa0t.cloudfront.net/Teams/${teamId}_TeamStats.json`
-    )
+    try {
+      const { data } = await axios.get(
+        `https://dn0a11v09sa0t.cloudfront.net/Teams/${teamId}_TeamStats.json`
+      )
+    } catch (err) {
+      console.log(err)
+    }
 
     this.setState({ players: data.Players })
   }
 
   render() {
-    console.log('teamId', this.props.navigation.getParam('teamId'))
     return (
       <ScrollView style={styles.container}>
         {this.state.players.map(player => {
           const { FirstName, LastName, TeamId } = player
+
           return (
             <Text
               onPress={() =>
                 this.props.navigation.navigate('IndividualPlayer', {
                   player,
-                  name: `${FirstName} ${LastName}`,
-                  teamId: TeamId
+                  name: `${FirstName} ${LastName}`
                 })
               }
             >
